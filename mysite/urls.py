aaa833de308997af5b81737f8407a09914dd11ee
arian-nj <arian.aghajani01@gmail.com>
2022-11-06 09:21:34 +0330

    simple index and detail view

--- mysite/urls.py
+++ mysite/urls.py
@@ -14,8 +14,10 @@ Including another URLconf
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
 from django.contrib import admin
-from django.urls import path
+from django.urls import path,include
+import polls.urls as polls_view
 
 urlpatterns = [
     path('admin/', admin.site.urls),
+    path('polls/',include(polls_view))
 ]
